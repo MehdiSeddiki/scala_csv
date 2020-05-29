681de071e48f1ff4e15e097aa00299463a9378b1
Mehdi SEDDIKI <mehdi.seddiki@epita.fr>
2020-05-30 00:32:04 +0200

    [UPD] Complete the menu in main function // Need to Clean the code

--- Project_CSV/src/main/scala/csv_main/main.scala
+++ Project_CSV/src/main/scala/csv_main/main.scala
@@ -3,15 +3,46 @@ package csv_main
 object main {
 
   def main(args: Array[String]): Unit = {
-      println("1. Query (Q)")
-      println("2. Reports (R)")
-      println("3. Exit the program (E)")
+      println("Q. Query")
+      println("R. Reports")
+      println("E. Exit the program")
       Iterator.continually(io.StdIn.readLine)
-        .takeWhile(_ != "E")
-        .foreach{
-          case "Q" => println("Query selectionned")
-          case "R" => println("Reports selectionned")
-          case _  => println("Bad choice, try again !")
+        .foreach {
+          case "Q" => {
+            println("1. Display the airports & runways at each airports")
+            println("2. Step Back")
+            Iterator.continually(io.StdIn.readLine)
+              .foreach {
+                case "1" => println("Please type a country name or its code")
+                case "2" => main(args)
+              }
+          }
+          case "R" => {
+            println("1. Country with the Highest number of airports & Lowest number of airports")
+            println("2. Type of runways per country")
+            println("3. Top 10 common runway latitude")
+            println("4. Step Back (B)")
+            Iterator.continually(io.StdIn.readLine)
+              .foreach {
+                case "1" => {
+                  println("you choose Highest nb")
+                  main(args)
+                }
+                case "2" => {
+                  println(" you choose type of runways")
+                  main(args)
+                }
+                case "3" => {
+                  println("you choose 10 common runway latitude")
+                  main(args)
+                  }
+                case "B" => main(args)
+                case _ => println("Bad choice, try again !")
+
+                }
+              }
+          case "E" => System.exit(0)
+          case _ => println("Bad choice try again !")
         }
   }
 }
