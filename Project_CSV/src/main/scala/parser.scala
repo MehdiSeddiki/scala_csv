1ea5664bc72685740e726d3267fdeb91ddfc9636
Julien Collos <julien.collos@epita.fr>
2020-05-30 11:37:53 +0200

    debut parser

--- Project_CSV/src/main/scala/parser.scala
+++ Project_CSV/src/main/scala/parser.scala
@@ -6,3 +6,15 @@ object parser {
   // la ligne en dessous permet de dire je veux recuper tel et tel colonne du fichier csv.
   airports.map(x => List(x(1), x(2), x(3), x(4), x(5)))
 }
+
+def parser(line: String): Option[Any] = {
+    line.map(_.split(",").toList) match {
+        case List(nos datas) => Some(List(nos data))
+        case _ => None
+    }
+}
+
+def readData(fileName:String): List[Any] = {
+    val line = Source.fromFile(filename).getLines.toList
+    val data = parseCsvLine(line)
+}
