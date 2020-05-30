bd9e583e3fe2e332eef43a65a700fce59a26132b
Julien Collos <julien.collos@epita.fr>
2020-05-30 17:21:24 +0200

    [UPD] sotck name & code countries 's + comment

--- Project_CSV/src/main/scala/parser.scala
+++ Project_CSV/src/main/scala/parser.scala
@@ -1,11 +1,20 @@
 import scala.io.Source
-
+/* j'ai cherche des trucs mais je n'arrive pas a voir comment ca va se profiler. Si j'ai bien compris, on parse les 3 fichiers
+, j'ai recupere les colonnes du nom du pays ou de son code. Apres je ne vois pas ce qu'il faut faire. Pour la BDD j'ai pas reussi
+a trouve comment on faisait sans spark et pour les classe mutable, on pourrai faire correspondre un attribut(List[le type associe])
+à une colonne de notre fichier csv
+ */
 object parser {
 
   val airports = Source.fromFile("airports.csv").getLines.map(_.split(",").toList)
-  val id_airport = airports.map(x => List(x(1)))
-  val name_airport = airports.map(x => List(x(4)))
+  val countries = Source.fromFile("countries.csv").getLines.map(_.split(",").toList)
+  val runways = Source.fromFile("runways.csv").getLines.map(_.split(",").toList)
 
+  val id_airport = airports.map(x => List(x(1)))
+  val name_airport = airports.map(x => List(x(4)))// si j'ai bien comrpis, je recupere la 4eme colonnes de airports
+  //mais si je veux travailler dessus je ne vois pas comment faire
+  val name_country = countries.map(x => List(x(3)))
+  val code_country = countries.map(x => List(x(2)))
   airports.map(x => List(x(1), x(2), x(3), x(4), x(5)))
 
 
@@ -22,6 +31,10 @@ object parser {
     val data = parserfile(line) // probleme de type ici car line est de type list et non de type string
   }
 }
+
+
+
+
 
 
 
