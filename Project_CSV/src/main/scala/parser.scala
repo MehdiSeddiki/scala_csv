163fbd3a34ea178e4d93628d276cbc69bdbc3408
Julien Collos <julien.collos@epita.fr>
2020-05-30 16:42:21 +0200

    [ADD] 2 functions to parse maybe

--- Project_CSV/src/main/scala/parser.scala
+++ Project_CSV/src/main/scala/parser.scala
@@ -3,18 +3,25 @@ import scala.io.Source
 object parser {
 
   val airports = Source.fromFile("airports.csv").getLines.map(_.split(",").toList)
-  // la ligne en dessous permet de dire je veux recuper tel et tel colonne du fichier csv.
+  val id_airport = airports.map(x => List(x(1)))
+  val name_airport = airports.map(x => List(x(4)))
+
   airports.map(x => List(x(1), x(2), x(3), x(4), x(5)))
-}
 
-def parser(line: String): Option[Any] = {
-    line.map(_.split(",").toList) match {
-        case List(nos datas) => Some(List(nos data))
-        case _ => None
+
+  //peut etre un truc comme ca en combinant nos deux trucs.
+  def parserfile(line: String): Option[Any] = {
+    line.split(",").toList match {
+      case List("nos datas") => Some(List("nos data"))
+      case _ => None
     }
+  }
+
+  def readData(fileName:String): List[Any] = {
+    val line = Source.fromFile(fileName).getLines.toList
+    val data = parserfile(line) // probleme de type ici car line est de type list et non de type string
+  }
 }
 
-def readData(fileName:String): List[Any] = {
-    val line = Source.fromFile(filename).getLines.toList
-    val data = parseCsvLine(line)
-}
+
+
