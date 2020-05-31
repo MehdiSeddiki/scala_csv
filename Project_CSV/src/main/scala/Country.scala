fb160141a4cd89bc5092735e42a29daa4e59cc5c
Mehdi SEDDIKI <mehdi.seddiki@epita.fr>
2020-05-31 22:47:11 +0200

    [UPD] Return a Country w/ 'None' type if the last column is empty

--- Project_CSV/src/main/scala/Country.scala
+++ Project_CSV/src/main/scala/Country.scala
@@ -10,18 +10,20 @@ object main{
     def makeCountry(line: String): Option[Countries] = {
 
       val l = line.split(",")
-      try {
-        val id = l(0).toInt
-        val code = l(1)
-        val name = l(2)
-        val continent = l(3)
-        val wikipedia_link = l(4)
-        val keyword = l(5)
-        Some(Countries(id, code, name,  continent, wikipedia_link, Some(keyword)))
-      }catch{
-        case e: Exception => None
+        try {
+          val id = l(0).toInt
+          val code = l(1)
+          val name = l(2)
+          val continent = l(3)
+          val wikipedia_link = l(4)
+          if (l(4) == l.last)
+            Some(Countries(id, code, name, continent, wikipedia_link, None))
+          else
+            Some(Countries(id, code, name, continent, wikipedia_link, Some(l(5))))
+        } catch {
+          case e: Exception=> None
+        }
       }
-    }
     val filename_countries = "resources/countries.csv"
     val countries = Source.fromFile(filename_countries).getLines.map(makeCountry)
     countries.foreach(println)
