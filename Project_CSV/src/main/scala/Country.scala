237476b36f8367a7f1393cf2cd42d030777c0bff
Mehdi SEDDIKI <mehdi.seddiki@epita.fr>
2020-05-31 22:35:47 +0200

    [UPD] Store data into case class

--- Project_CSV/src/main/scala/Country.scala
+++ Project_CSV/src/main/scala/Country.scala
@@ -1,23 +1,30 @@
 import scala.io.Source
 
+object main{
+
+  def main(args: Array[String]): Unit = {
 
 
+    case class Countries(id: Int, code: String, name: String, continent: String, wikipedia_link: String, keyword: Option[String] = None)
 
-class Country{
+    def makeCountry(line: String): Option[Countries] = {
 
-  def print_data(l: List[Any]) = println(l)
-  def get_code() = country.code
-  def get_name() = country.name
-}
+      val l = line.split(",")
+      try {
+        val id = l(0).toInt
+        val code = l(1)
+        val name = l(2)
+        val continent = l(3)
+        val wikipedia_link = l(4)
+        val keyword = l(5)
+        Some(Countries(id, code, name,  continent, wikipedia_link, Some(keyword)))
+      }catch{
+        case e: Exception => None
+      }
+    }
+    val filename_countries = "resources/countries.csv"
+    val countries = Source.fromFile(filename_countries).getLines.map(makeCountry)
+    countries.foreach(println)
+  }
 
-object country{
-  val filename_countries = "resources/countries.csv"
-  val countries = Source.fromFile(filename_countries).getLines.map(_.split(",").toList)
-  //val stock = airports.map(x => List(x(1), x(2), x(3), x(4), x(5)))
-  val id = countries.map(x => List(x(1))).toList
-  val code = countries.map(x => List(x(2))).toList
-  val name = countries.map(x => List(x(3))).toList
-  val continent = countries.map(x => List(x(4))).toList
-  val wikipedia_link = countries.map(x => List(x(5))).toList
-  val keyword = countries.map(x => List(x(6))).toList
 }
